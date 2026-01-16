be70cdffe29c6d07384babd31ad2a9434149d561
Winniper <debadityabarman12@gmail.com>
2026-01-16 12:35:44 +0530

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import Navbar from '@components/navbar'
 
 function App() {
 
   return (
-    <h1 className='font-bold'>
-      Hello world!!
-    </h1>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src='/images/logo.svg' alt='logo.png'/>
+        <p className='font-bold'>DevBarman's Portfolio</p>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
