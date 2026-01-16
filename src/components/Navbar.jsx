8e7407806b0fa5172f46daa13b2b956713a7c42a
Winniper <debadityabarman12@gmail.com>
2026-01-16 12:56:32 +0530

    added Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { navLinks,navIcons } from '@constants'
+
+import dayjs from 'dayjs'
 
 const Navbar = () => {
   return (
@@ -6,6 +9,22 @@ const Navbar = () => {
       <div>
         <img src='/images/logo.svg' alt='logo.png'/>
         <p className='font-bold'>DevBarman's Portfolio</p>
+        <ul>
+          {navLinks.map(({ id, name }) =>(
+          <li key={id}>
+            <p>{name}</p>
+          </li>))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img className="icon-hover" src={img} alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
       </div>
     </nav>
   )
