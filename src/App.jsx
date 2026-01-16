f9cfca998df4cbdd26c666ff93bcc5518682fb56
Winniper <debadityabarman12@gmail.com>
2026-01-16 23:33:25 +0530

    added the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import Navbar from '@components/Navbar'
+import { Navbar, Welcome } from "@components"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
   return (
     <main>
       <Navbar />
+      <Welcome />
+      <Analytics />
     </main>
   )
 }
